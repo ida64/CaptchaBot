72a7f65d68e078ca0b0e8213ab74520ba1549b67
Omar <paging@tuta.io>
2025-07-10 06:19:09 -0500

    Add parts of localization support

--- config.py
+++ config.py
@@ -26,6 +26,24 @@ class Config:
         with open(self.filename, 'w') as f:
             yaml.dump(self.data, f)
 
+    def save_default_locale_en_us(self) -> None:
+        default_locale_data = CommentedMap({
+            'verification_view': CommentedMap({
+                'embed': CommentedMap({
+                    'title': "Captcha Verification",
+                    'description': "Click the button below to complete a captcha challenge.\nIf the challenge is too difficult to read, click the button again to get a new one.",
+                }),
+                'button': CommentedMap({
+                    'label': ":robot: Start Verification",
+                }),
+            }),
+        })
+
+        localization_dir = "localization"
+        locale_file = f"{localization_dir}/en_US.yaml"
+        with open(locale_file, 'w') as f:
+            yaml.dump(default_locale_data, f)
+
     def save_default(self) -> None:
         default_data = CommentedMap({
             'discord': CommentedMap({
@@ -38,11 +56,13 @@ class Config:
                     'verified_role_id': 0,
                     'log_channel_id': 0,
                 }),
+                'localization_dir': 'localization',
             }),
         })
+        self.data = default_data
+        self.save()
+        self.save_default_locale_en_us()
 
-        with open(self.filename, 'w') as f:
-            yaml.dump(default_data, f)
 
     def get(self, key: str, default=None):
         keys = key.split('.')
@@ -53,3 +73,7 @@ class Config:
             else:
                 return default
         return value if value is not None else default
+
+if __name__ == "__main__":
+    config = Config.load_config('config.yaml')
+    print(config.data)
